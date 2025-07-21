f33e2034c3ad13f367998975af6ec5040de318e6
Quan Pham <doraemonfanclub@gmail.com>
2025-07-21 20:42:36 +0700

    add search data

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,15 @@
-import { createFromSource } from "fumadocs-core/search/server";
-import { source } from "@/lib/source";
+import { createSearchAPI } from "fumadocs-core/search/server";
+import { blog, source } from "@/lib/source";
 
-export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/open-source/supported-languages
+const combinedSrcs = [...source.getPages(), ...blog.getPages()];
+
+export const { GET } = createSearchAPI("advanced", {
   language: "english",
+  indexes: combinedSrcs.map((page) => ({
+    title: page.data.title,
+    description: page.data.description,
+    url: page.url,
+    id: page.url,
+    structuredData: page.data.structuredData,
+  })),
 });
