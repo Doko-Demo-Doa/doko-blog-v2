31e8aa3837d8ca8eb1e3169b84ae2c509df36081
Quan Pham <doraemonfanclub@gmail.com>
2025-07-22 09:23:43 +0700

    change to space indents

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,5 +3,5 @@ import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
 }
