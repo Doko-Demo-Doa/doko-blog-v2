e293b0be7efb2ffb8af87390cddb5a46674af424
Quan Pham <doraemonfanclub@gmail.com>
2025-07-22 10:00:22 +0700

    react testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co"],
+  },
 };
 
 export default withMDX(config);
